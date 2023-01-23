9fcffb4ec7fd3b5ead6e0911d1a9d5f7b9c5c132
ClearCB <abelcasasccb@gmail.com>
2023-01-23 19:08:28 +0100

    feat(Bicicleta): add nueva clase bicicleta

--- bicipalma/src/domain/bicicleta/Bicicleta.java
+++ bicipalma/src/domain/bicicleta/Bicicleta.java
@@ -3,8 +3,23 @@ package domain.bicicleta;
 public class Bicicleta {
     int id;
 
+    // Constructor
     Bicicleta(int id){
         this.id = id;
     }
-    
+
+    // Getters y setters
+    public int getId(){
+        return this.id;
+    }
+
+    public void setId(int value){
+        this.id = value;
+    }
+
+    // Métodos
+    @Override
+    public String toString(){
+        return ("La bicleta tiene el id: "+ this.getId());
+    }
 }
